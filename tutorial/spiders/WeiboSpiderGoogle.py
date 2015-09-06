7a79f41d1d01a7db24d76d66b6fa82f329111b61
mawentao007 <1320963812@qq.com>
2015-09-07 01:54:33 +0800

    可以实现抓取某人所有微博的功能了

--- tutorial/spiders/WeiboSpiderGoogle.py
+++ tutorial/spiders/WeiboSpiderGoogle.py
@@ -17,7 +17,7 @@ class WeiboSpider(scrapy.Spider):
 #选取请求头部很重要，这里选取的是谷歌的爬虫头部
         user_agent = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F70 Safari/600.1.4 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
 
-        sumainUrl = "http://m.weibo.cn/u/2118407073"
+        #sumainUrl = "http://m.weibo.cn/u/2118407073"
         suurlBase = "http://m.weibo.cn/page/json?containerid=1005052118407073_-_WEIBO_SECOND_PROFILE_WEIBO&page="
         sufirstToParse = "http://m.weibo.cn/page/tpl?containerid=1005052118407073_-_WEIBO_SECOND_PROFILE_WEIBO&itemid=&title=%E5%85%A8%E9%83%A8%E5%BE%AE%E5%8D%9A"
 
@@ -30,11 +30,16 @@ class WeiboSpider(scrapy.Spider):
 #第一个请求发出并获得结果，默认回调parse函数
         def start_requests(self):
             #起始url可以为空
-            return [scrapy.FormRequest( "http://",
+            return [scrapy.FormRequest( "http://m.weibo.cn/u/2118407073",
                                     headers=self.user_agent)]
 
         def parse(self, response):
-            for  page in range(1,3):
+            data = self.parse_html_json(response)
+            postNumStr = data['stage']['page'][1]['mblogNum']     #获取微博总数并计算需要抓取的页面数量
+            postNum = int(postNumStr)
+            pageNum = (postNum % 10 + 9) / 10 + postNum/10
+
+            for  page in range(1,pageNum+1):
                 if page is 1:
                     yield scrapy.Request(self.firstToParse,headers=self.user_agent,callback=self.parse_html_contents)
                 else:
@@ -42,28 +47,30 @@ class WeiboSpider(scrapy.Spider):
                     yield scrapy.Request(url,headers=self.user_agent,callback=self.parse_json_contents)
 
 
-
-
-
-
-
 #该函数首先通过soup从html中解析出包含json数据的元素
 #之后通过re将相应的json字符串获取到
 #最后利用json将其载入
-        def parse_html_contents(self,response):
+
+        def parse_html_json(self,response):
                 soup = BeautifulSoup(response.body_as_unicode(),"lxml")
                 script = soup.find('script', text=re.compile('render_data'))
                 #注意json必须要双引号
                 json_text = re.search(r'\$render_data\s*=\s*({.*?})\s*;$',
                        script.string, flags=re.DOTALL | re.MULTILINE).group(1).replace("'", '"')
                 data = json.loads(json_text)
+                return data
+
+
+#解析包含json的html的页面的内容，要调用parse_html_json
+        def parse_html_contents(self,response):
+                data = self.parse_html_json(response)
                 for num in range(0,len(data['stage']['page'][1]['card_group'])):
                      item = WeiboItem()
-                     item['post'] =  data['stage']['page'][1]['card_group'][num]['mblog']['text']
+                     item['post'] = data['stage']['page'][1]['card_group'][num]['mblog']['text']
                      yield item
 
 
-
+#解析response为json类型数据
         def parse_json_contents(self, response):
                 jsonresponse = json.loads(response.body_as_unicode())
                 for num in range(0,len(jsonresponse["cards"][0]["card_group"])):
