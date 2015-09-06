08a1b4cc33ae20153e54186a14442dabe14f2c49
mawentao007 <1320963812@qq.com>
2015-09-06 11:30:06 +0800

    googleSpider

--- tutorial/spiders/WeiboSpiderGoogle.py
+++ tutorial/spiders/WeiboSpiderGoogle.py
@@ -7,6 +7,7 @@
 import scrapy
 import re
 import time
+import json
 from tutorial.WeiboItem import WeiboItem
 
 class WeiboSpider(scrapy.Spider):
@@ -14,10 +15,6 @@ class WeiboSpider(scrapy.Spider):
 
         user_agent = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F70 Safari/600.1.4 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
 
-        _cookie={"SUB":"_2A2547rshDeTxGeRP7FcU9CzLyj2IHXVYEMVprDV6PUJbrdAKLXH3kW2bdy6UqyJs-c8G8GcX7xITFuIkGg..",
-                " SUHB":"0pHgiRk5AUsN1y",
-                "_T_WM":"f3cd96c8a1bdcf92fe154ac20040a56e",
-                "gsid_CTandWM":"4ulE23bf1M9obnQWHqZWv97Xobt"}
 
 
         def start_requests(self):
@@ -25,15 +22,28 @@ class WeiboSpider(scrapy.Spider):
                                     headers=self.user_agent)]
 
         def parse(self, response):
-            for  page in range(2,10):
-                url = "http://m.weibo.cn/page/json?containerid=1005051799756001_-_WEIBO_SECOND_PROFILE_WEIBO&page=" + str(page)
-                yield scrapy.Request(url,headers=self.user_agent,callback=self.parse_dir_contents)
+                jsonresponse = json.loads(response.body_as_unicode())
+                for num in range(2,len(jsonresponse["cards"][0]["card_group"])):
+                    mblog = jsonresponse["cards"][0]["card_group"][num]["mblog"]
+                    item = WeiboItem()
+                    if mblog.has_key("retweeted_status"):
+                                    item["post"]=mblog["retweeted_status"]["text"]
+                    else:
+                                    item["post"]=mblog["text"]
+
+
+                    yield item
+            # for  page in range(1,2):
+            #     url = "http://m.weibo.cn/page/json?containerid=1005051799756001_-_WEIBO_SECOND_PROFILE_WEIBO&page=" + str(page)
+            #     yield scrapy.Request(url,headers=self.user_agent,callback=self.parse_dir_contents)
 
         def parse_dir_contents(self, response):
-            for sel in response.xpath('//span[@class="ctt"]'):
-                item = WeiboItem()
-                item['post'] = sel.xpath('text()').extract()[0].strip()
-                yield item
+                jsonresponse = json.loads(response.body_as_unicode())
+                for ans in jsonresponse["cards"]:
+                    item = WeiboItem()
+                    item["post"] = ans["mblog"]["text"]
+                    yield item
+
 
 
 
